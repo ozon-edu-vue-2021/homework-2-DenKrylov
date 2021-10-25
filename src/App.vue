fe2cb8cfe3c1c87ca6d57f70dc4ee59ace5a0b11
DenKrylov <deniskrilov87@gmail.com>
2021-10-25 23:02:16 +0300

    output all line

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
   <div id="app">
-    <Tree />
+    <ul id="tree" class="tree">
+      <tree :item="data"></tree>
+    </ul>
   </div>
 </template>
 
 <script>
+import Data from '../public/static/node_modules1.json';
 import Tree from './components/Tree.vue';
 
 export default {
   name: 'App',
-  components: {
-    Tree
-  }
+  data: function() {
+		return {
+			data: Data,
+		}
+	},
+	components: {
+		Tree,
+	}
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: rgba(49, 54, 59, 1);
+  color: rgb(65, 184, 131);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: rgb(53, 73, 94);
-  display: flex;
 }
 </style>
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,22 +1,93 @@
 <template>
-	<ul id="tree">
-		<tree-branch :item="data"></tree-branch>
-	</ul>
+	<li id="tree">
+		<div :class="branch()" @click="action()">
+			<img v-bind:src="getIcon()" :alt="getType()">
+			<span>{{ name }}</span>
+		</div>
+		<ul v-show="isOpen">
+			<tree
+				v-for="(el, i) in item.contents"
+				:key="i"
+				:item=el
+			></tree>
+		</ul>
+	</li>
 </template>
 
 <script>
-import Data from '../../public/static/node_modules.json';
-import TreeBranch from './TreeBranch.vue';
 
 export default{
 	name: 'tree',
+	props: {
+		item: Object,
+	},
 	data: function() {
 		return {
-			data: Data,
+			isOpen: false,
+			name: this.item.name,
+			type: this.item.type,
 		}
 	},
-	components: {
-		TreeBranch,
+	methods: {
+		getIcon: function() {
+			if(this.type == 'directory') {
+				return 'assets/folder.svg';
+			} else if(this.type == 'file') {
+				return 'assets/description.svg';
+			} else if(this.type == 'link') {
+				return 'assets/link.svg';
+			}
+		},
+		getType: function() {
+			if(this.type == 'directory') {
+				return 'folder';
+			} else if(this.type == 'file') {
+				return 'file';
+			} else if(this.type == 'link') {
+				return 'link';
+			}
+		},
+		action: function() {
+			if(this.type == 'directory') {
+				this.isOpen = !this.isOpen;
+			} else if(this.type != 'directory') {
+				document.getSelection().setBaseAndExtent();
+			}
+		},
+		branch: function() {
+			if(this.type == 'directory') {
+				return 'folder branch';
+			} else {
+				return 'branch';
+			}
+		}
 	}
 }
 </script>
+
+<style scoped>
+li {
+	display: flex;
+	flex-direction: column;
+	margin-left: 25px;
+	height: 25px;
+	cursor: pointer;
+}
+.branch {
+	display: flex;
+	align-items: center;
+}
+span::selection {
+    color: rgba(49, 54, 59, 1);
+	background: rgb(65, 184, 131);
+}
+.folder {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+</style>
